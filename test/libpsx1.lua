ebcf6cbe9edaf6acfe588e7b3be787403b580be0
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2024-03-16 23:00:48 -0300

    Add warning about libpsx bug

---
+++ test/libpsx1.lua
@@ -0,0 +1,35 @@
+-- there's a bug in libpsx preventing it from work on C++ projects:
+-- https://bugzilla.kernel.org/show_bug.cgi?id=218607
+
+local fs = require 'filesystem'
+local file = require 'file'
+local stream = require 'stream'
+local system = require 'system'
+local inbox = require 'inbox'
+
+if _CONTEXT == 'main' then
+    local vm2 = spawn_vm{
+        module = '.',
+        inherit_context = false,
+        concurrency_hint = 1,
+        new_master = true
+    }
+    vm2:send(inbox)
+    inbox:receive()
+else
+    system.set_no_new_privs()
+    local vm1 = inbox:receive()
+    vm1:send(true)
+end
+
+local status = stream.scanner.new{ field_separator = ':\t' }
+status.stream = file.stream.new()
+status.stream:open(
+    fs.path.new('/proc/thread-self/status'), file.open_flag.read_only)
+while true do
+    local fields = status:get_line()
+    if tostring(fields[1]) == 'NoNewPrivs' then
+        print(fields[2])
+        break
+    end
+end
