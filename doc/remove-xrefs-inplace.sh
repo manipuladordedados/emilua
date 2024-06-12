0f66084ea5ce29cd8398080c1e465a3c3927afcc
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2024-06-12 12:27:51 -0300

    Fix doc/remove-xrefs-inplace.sh
    New syntax works on DASH which hopefully is portable to every POSIX
    system.
    
    The POSIX specification for echo doesn't support any arguments. printf
    would be an option, but we can just use the desired character in the sed
    expression directly.

--- doc/remove-xrefs-inplace.sh
+++ doc/remove-xrefs-inplace.sh
@@ -7,4 +7,4 @@ grep -Fre 'xref:' modules/tutorial/pages/*.adoc | grep -Eve 'xref:(tutorial|ref)
 
 sed -i -E 's/xref(:ref)?:(.*)\.adoc\[([^]]*)\]/<<_\2,\3>>/g' modules/ref/pages/*.adoc modules/tutorial/pages/*.adoc
 sed -i -E 's/<<_([^,]*)[.:]/<<_\1_/g' modules/ref/pages/*.adoc modules/tutorial/pages/*.adoc
-sed -i 's/$(echo -ne '\u2714')/icon:check[2x]/g' modules/ref/pages/*.adoc modules/tutorial/pages/*.adoc
+sed -i 's/✔/icon:check[2x]/g' modules/ref/pages/*.adoc modules/tutorial/pages/*.adoc
