8b6ddac480331ce35160b12258e25a9a39264204
Valter Nazianzeno <manipuladordedados@gmail.com>
2024-06-12 02:24:49 +0000

    add remove-xrefs-inplace script

---
+++ doc/remove-xrefs-inplace.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+
+# Check for xref issues
+grep -Fre 'xref:' modules/tutorial/pages/*.adoc | grep -Eve 'xref:(tutorial|ref):' && exit 1
+
+sed -i -E 's/xref(:ref)?:(.*)\.adoc\[([^]]*)\]/<<_\2,\3>>/g' modules/ref/pages/*.adoc modules/tutorial/pages/*.adoc
+sed -i -E 's/<<_([^,]*)[.:]/<<_\1_/g' modules/ref/pages/*.adoc modules/tutorial/pages/*.adoc
+sed -i 's/$(echo -ne '\u2714')/icon:check[2x]/g' modules/ref/pages/*.adoc modules/tutorial/pages/*.adoc
