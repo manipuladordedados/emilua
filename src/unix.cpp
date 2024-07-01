9dba4183f3083d518c2ee43054920c2a378eb137
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2024-07-01 19:17:12 -0300

    Set O_NONBLOCK for UNIX sockets upon release

--- src/unix.cpp
+++ src/unix.cpp
@@ -847,6 +847,13 @@ static int unix_datagram_socket_release(lua_State* L)
         return lua_error(L);
     }
 
+    {
+        asio_error_code ignored_ec;
+        // Boost.Asio doesn't set O_NONBLOCK on open, so we do it before the
+        // user try to use this handle in Capsicum-backed environments.
+        sock->socket.native_non_blocking(true, ignored_ec);
+    }
+
     asio_error_code ec;
     int rawfd = sock->socket.release(ec);
     BOOST_SCOPE_EXIT_ALL(&) {
@@ -2241,6 +2248,13 @@ static int unix_stream_socket_release(lua_State* L)
     if (sock->socket.native_handle() == INVALID_FILE_DESCRIPTOR) {
         push(L, std::errc::bad_file_descriptor);
         return lua_error(L);
+    }
+
+    {
+        asio_error_code ignored_ec;
+        // Boost.Asio doesn't set O_NONBLOCK on open, so we do it before the
+        // user try to use this handle in Capsicum-backed environments.
+        sock->socket.native_non_blocking(true, ignored_ec);
     }
 
     asio_error_code ec;
@@ -3647,6 +3661,13 @@ static int unix_stream_acceptor_release(lua_State* L)
         return lua_error(L);
     }
 
+    {
+        asio_error_code ignored_ec;
+        // Boost.Asio doesn't set O_NONBLOCK on open, so we do it before the
+        // user try to use this handle in Capsicum-backed environments.
+        acceptor->native_non_blocking(true, ignored_ec);
+    }
+
     asio_error_code ec;
     int rawfd = acceptor->release(ec);
     BOOST_SCOPE_EXIT_ALL(&) {
@@ -4189,6 +4210,13 @@ static int unix_seqpacket_socket_release(lua_State* L)
     if (sock->socket.native_handle() == INVALID_FILE_DESCRIPTOR) {
         push(L, std::errc::bad_file_descriptor);
         return lua_error(L);
+    }
+
+    {
+        asio_error_code ignored_ec;
+        // Boost.Asio doesn't set O_NONBLOCK on open, so we do it before the
+        // user try to use this handle in Capsicum-backed environments.
+        sock->socket.native_non_blocking(true, ignored_ec);
     }
 
     asio_error_code ec;
@@ -5623,6 +5651,13 @@ static int unix_seqpacket_acceptor_release(lua_State* L)
     if (acceptor->native_handle() == INVALID_FILE_DESCRIPTOR) {
         push(L, std::errc::bad_file_descriptor);
         return lua_error(L);
+    }
+
+    {
+        asio_error_code ignored_ec;
+        // Boost.Asio doesn't set O_NONBLOCK on open, so we do it before the
+        // user try to use this handle in Capsicum-backed environments.
+        acceptor->native_non_blocking(true, ignored_ec);
     }
 
     asio_error_code ec;
