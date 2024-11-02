83c137a446757b2af4897133cafd9a847714dd1d
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2024-11-02 17:49:59 -0300

    [libmain] Move default parse_args() to parse_emilua_bin_args()
    Most likely only /usr/bin/emilua itself is going to use
    parse_emilua_bin_args() so the default parse_args() should be more
    useful to the common case.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 // Copyright (c) 2024 Vinícius dos Santos Oliveira
 // SPDX-License-Identifier: MIT OR BSL-1.0
 
-namespace emilua::main {
+namespace emilua {
+
+class app_context;
+
+namespace main {
 int main(int argc, char *argv[], char *envp[]);
-} // namespace emilua::main
+void parse_emilua_bin_args(int argc, char *argv[], app_context& appctx);
+
+void parse_args(int argc, char *argv[], app_context& appctx)
+{
+    return parse_emilua_bin_args(argc, argv, appctx);
+}
+
+} // namespace main
+} // namespace emilua
 
 int main(int argc, char *argv[], char *envp[])
 {
