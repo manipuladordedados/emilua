0997bb7a61bbcdcd3b1d44f6ae4e3bb1e899a2aa
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2025-01-20 18:49:29 -0300

    Parameterize namespace use in luabcxxd

--- tool/luabcxxd.cpp
+++ tool/luabcxxd.cpp
@@ -1,3 +1,4 @@
+#include <string_view>
 #include <filesystem>
 #include <iostream>
 #include <fstream>
@@ -28,9 +29,17 @@ std::string load_file(std::string path)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3) {
+    if (argc != 3 && argc != 4) {
         std::cerr << argv[0] << " <input-file> <output-file>\n";
+        std::cerr << argv[0] << " <input-file> <output-file> <namespace>\n";
         return 1;
+    }
+
+    std::string_view namespace_;
+    if (argc == 4) {
+        namespace_ = argv[3];
+    } else {
+        namespace_ = "emilua";
     }
 
     const std::string id = std::filesystem::path{std::string{argv[1]}}.stem()
@@ -63,7 +72,7 @@ int main(int argc, char *argv[])
     out <<
         "#include <cstddef>\n" <<
         "\n" <<
-        "namespace emilua {\n" <<
+        "namespace " << namespace_ << " {\n" <<
         "unsigned char " << id << "_bytecode[] = {\n";
 
     if (len > 0) {
@@ -77,5 +86,5 @@ int main(int argc, char *argv[])
     out <<
         "\n};\n" <<
         "std::size_t " << id << "_bytecode_size = " << len << ";\n" <<
-        "} // namespace emilua\n";
+        "} // namespace " << namespace_ << "\n";
 }
