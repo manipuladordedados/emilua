manipuladordedados/emilua

commit f97b5be8b5550f73e79687fa04692ab0304c4665

Destroy io_context before waiting for other threads
This change makes no difference for Emilua core.

While developing a plugin to communicate with Qt's QCoreApplication
thread it became apparent that a service's destructor attached to
io_context can be an useful technique to interrupt extra threads. To
make the life of some plugin developers easier I'm pushing this commit.

This behavior already existed for io_context objects of other threads
(they were destroyed before the thread joining phase), and there's no
reason for making the behavior of the first io_context special in this
regards. Now the behavior is unified which is good anyways.

src/main.ypp
@@ -400,20 +400,6 @@ end:
         appctx.app_args.emplace_back(filename);
     }
 
-#if EMILUA_CONFIG_THREAD_SUPPORT_LEVEL == 2
-    asio::io_context ioctx{main_ctx_concurrency_hint};
-    asio::make_service<emilua::properties_service>(
-        ioctx, main_ctx_concurrency_hint);
-#elif EMILUA_CONFIG_THREAD_SUPPORT_LEVEL == 1
-    asio::io_context ioctx{1};
-    asio::make_service<emilua::properties_service>(ioctx, 1);
-#elif EMILUA_CONFIG_THREAD_SUPPORT_LEVEL == 0
-    asio::io_context ioctx{BOOST_ASIO_CONCURRENCY_HINT_UNSAFE};
-    asio::make_service<emilua::properties_service>(ioctx, 1);
-#else
-# error Invalid thread support level
-#endif
-
     if (
         auto it = appctx.app_env.find("EMILUA_PATH") ;
         it != appctx.app_env.end()
@@ -452,26 +438,42 @@ end:
         "emilua-" BOOST_PP_STRINGIZE(EMILUA_CONFIG_VERSION_MAJOR)
         "." BOOST_PP_STRINGIZE(EMILUA_CONFIG_VERSION_MINOR);
 
-    try {
-        auto vm_ctx = emilua::make_vm(ioctx, appctx,
-                                      emilua::widen_on_windows(filename),
-                                      main_context_type);
-        appctx.master_vm = vm_ctx;
-        vm_ctx->strand().post([vm_ctx]() {
-            vm_ctx->fiber_resume(
-                vm_ctx->L(),
-                hana::make_set(
-                    emilua::vm_context::options::skip_clear_interrupter));
-        }, std::allocator<void>{});
-    } catch (std::exception& e) {
-        try {
-            boost::nowide::cerr << "Error starting the lua VM: " << e.what() <<
-                std::endl;
-        } catch (const std::ios_base::failure&) {}
-        return 1;
-    }
+    {
+#if EMILUA_CONFIG_THREAD_SUPPORT_LEVEL == 2
+        asio::io_context ioctx{main_ctx_concurrency_hint};
+        asio::make_service<emilua::properties_service>(
+            ioctx, main_ctx_concurrency_hint);
+#elif EMILUA_CONFIG_THREAD_SUPPORT_LEVEL == 1
+        asio::io_context ioctx{1};
+        asio::make_service<emilua::properties_service>(ioctx, 1);
+#elif EMILUA_CONFIG_THREAD_SUPPORT_LEVEL == 0
+        asio::io_context ioctx{BOOST_ASIO_CONCURRENCY_HINT_UNSAFE};
+        asio::make_service<emilua::properties_service>(ioctx, 1);
+#else
+# error Invalid thread support level
+#endif
 
-    ioctx.run();
+        try {
+            auto vm_ctx = emilua::make_vm(ioctx, appctx,
+                                          emilua::widen_on_windows(filename),
+                                          main_context_type);
+            appctx.master_vm = vm_ctx;
+            vm_ctx->strand().post([vm_ctx]() {
+                vm_ctx->fiber_resume(
+                    vm_ctx->L(),
+                    hana::make_set(
+                        emilua::vm_context::options::skip_clear_interrupter));
+            }, std::allocator<void>{});
+        } catch (std::exception& e) {
+            try {
+                boost::nowide::cerr << "Error starting the lua VM: " <<
+                    e.what() << std::endl;
+            } catch (const std::ios_base::failure&) {}
+            return 1;
+        }
+
+        ioctx.run();
+    }
 
     {
         std::unique_lock<std::mutex> lk{appctx.extra_threads_count_mtx};

commit 5a7215e6a226a19f02d50ea77dce053951eafea5

Fix spawn_context_threads() for the main context
The bug was introduced in commit
f97b5be8b5550f73e79687fa04692ab0304c4665 which brought a good change,
but inadvertently broke old code.

src/actor.ypp
@@ -2835,11 +2835,22 @@ static int spawn_context_threads(lua_State* L)
     luaL_checktype(L, 1, LUA_TNUMBER);
 
     auto& vm_ctx = get_vm_context(L);
+    auto ioctx = vm_ctx.ioctxref.lock();
     auto nthrds = lua_tointeger(L, 1);
+
+    // It should only happen if you're embedding the Emilua runtime inside 3rd
+    // party apps, and the app developer doesn't want to share means for
+    // extending the outer asio::io_context lifetime with Emilua (which is fine
+    // as the only limitation is that we won't be able to support
+    // spawn_context_threads() for this asio::io_context).
+    if (!ioctx) {
+        push(L, std::errc::not_supported);
+        return lua_error(L);
+    }
 
     int concurrency_hint;
     {
-        asio::config ioctxcfg{vm_ctx.strand().context()};
+        asio::config ioctxcfg{*ioctx};
         concurrency_hint = ioctxcfg.get("scheduler", "concurrency_hint", 1);
     }
     if (concurrency_hint == 1) {
@@ -2857,26 +2868,20 @@ static int spawn_context_threads(lua_State* L)
         vm_ctx.appctx.extra_threads_count += nthrds;
     }
     for (;nthrds > 0 ; --nthrds) {
-        std::thread{
-            [
-                &appctx=vm_ctx.appctx,
-                &ioctx=vm_ctx.strand().context(),
-                guard=vm_ctx.ioctxref.lock()
-            ]() mutable {
-                ioctx.run(); //< doesn't need a work guard
-                guard.reset();
+        std::thread{[&appctx=vm_ctx.appctx,ioctx]() mutable {
+            ioctx->run(); //< doesn't need a work guard
+            ioctx.reset();
 
-                std::unique_lock<std::mutex> lk{appctx.extra_threads_count_mtx};
-                --appctx.extra_threads_count;
-                if (appctx.extra_threads_count == 0) {
-                    std::notify_all_at_thread_exit(
-                        appctx.extra_threads_count_empty_cond, std::move(lk));
-                } else {
-                    std::notify_all_at_thread_exit(
-                        appctx.extra_threads_count_dummy_cond, std::move(lk));
-                }
+            std::unique_lock<std::mutex> lk{appctx.extra_threads_count_mtx};
+            --appctx.extra_threads_count;
+            if (appctx.extra_threads_count == 0) {
+                std::notify_all_at_thread_exit(
+                    appctx.extra_threads_count_empty_cond, std::move(lk));
+            } else {
+                std::notify_all_at_thread_exit(
+                    appctx.extra_threads_count_dummy_cond, std::move(lk));
             }
-        }.detach();
+        }}.detach();
     }
     return 0;
 #endif

src/ipc_actor.cpp
@@ -1300,15 +1300,17 @@ static int child_main(void*)
     }
 
     {
-        asio::io_context ioctx{main_ctx_concurrency_hint};
+        auto ioctx = std::make_shared<asio::io_context>(
+            main_ctx_concurrency_hint);
 
         try {
             auto vm_ctx = make_vm(
-                appctx, ioctx, ContextType::worker, entry_point, import_root);
+                appctx, *ioctx, ContextType::worker, entry_point, import_root);
+            vm_ctx->ioctxref = ioctx;
             appctx.master_vm = vm_ctx;
 
             ++vm_ctx->inbox.nsenders;
-            auto inbox_service = new ipc_actor_inbox_service{ioctx, inboxfd};
+            auto inbox_service = new ipc_actor_inbox_service{*ioctx, inboxfd};
             vm_ctx->pending_operations.push_back(*inbox_service);
 
             vm_ctx->strand().post([vm_ctx]() {
@@ -1325,7 +1327,7 @@ static int child_main(void*)
             return 1;
         }
 
-        ioctx.run();
+        ioctx->run();
     }
 
     {

src/lib_main.cpp
@@ -557,13 +557,15 @@ const asio::execution_context::service_maker& main_ctx_service_maker()
 #endif // BOOST_OS_WINDOWS
 
 #if BOOST_OS_WINDOWS
-void (*make_master_vm)(app_context& appctx, asio::io_context& ioctx) =
-    [](app_context& appctx, asio::io_context& ioctx)
+void (*make_master_vm)(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx) =
+    [](app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #else // BOOST_OS_WINDOWS
 # if defined(EMILUA_STATIC_BUILD)
 [[gnu::weak]]
 # endif // defined(EMILUA_STATIC_BUILD)
-void make_master_vm(app_context& appctx, asio::io_context& ioctx)
+void make_master_vm(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #endif // BOOST_OS_WINDOWS
 {}
 #if BOOST_OS_WINDOWS
@@ -712,7 +714,8 @@ int main(int argc, char *argv[], char *envp[])
     }
 
     {
-        asio::io_context ioctx{main_ctx_service_maker()};
+        auto ioctx = std::make_shared<asio::io_context>(
+            main_ctx_service_maker());
         try {
             make_master_vm(appctx, ioctx);
         } catch (std::exception& e) {
@@ -722,7 +725,7 @@ int main(int argc, char *argv[], char *envp[])
             } catch (const std::ios_base::failure&) {}
             return 1;
         }
-        run(appctx, ioctx);
+        run(appctx, *ioctx);
     }
 
     {

src/main.ypp
@@ -341,14 +341,18 @@ const asio::execution_context::service_maker& main_ctx_service_maker()
 }
 
 #if BOOST_OS_WINDOWS
-extern void (*make_master_vm)(app_context& appctx, asio::io_context& ioctx);
-static void make_master_vm2(app_context& appctx, asio::io_context& ioctx)
+extern void (*make_master_vm)(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx);
+static void make_master_vm2(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #else // BOOST_OS_WINDOWS
-void make_master_vm(app_context& appctx, asio::io_context& ioctx)
+void make_master_vm(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #endif // BOOST_OS_WINDOWS
 {
-    auto vm_ctx = make_vm(appctx, ioctx, main_context_type,
+    auto vm_ctx = make_vm(appctx, *ioctx, main_context_type,
                           widen_on_windows(filename));
+    vm_ctx->ioctxref = ioctx;
     appctx.master_vm = vm_ctx;
     vm_ctx->strand().post([vm_ctx]() {
         vm_ctx->fiber_resume(

tool/genmain.cpp
@@ -138,11 +138,12 @@ stream.write_all(system.out, byte_span.append(
     'namespace emilua::main {\n' ..
     'int main(int argc, char *argv[], char *envp[]);\n' ..
     [[
-    void make_master_vm(app_context& appctx, asio::io_context& ioctx)
+    void make_master_vm(app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
     {
         auto vm_ctx = make_vm(
-            appctx, ioctx, ContextType::main,
+            appctx, *ioctx, ContextType::main,
             fs::path{"/dev/null/NUL/app/init.lua", fs::path::generic_format});
+        vm_ctx->ioctxref = ioctx;
         appctx.master_vm = vm_ctx;
         vm_ctx->strand().post([vm_ctx]() {
             vm_ctx->fiber_resume(
@@ -191,15 +192,19 @@ int main(int argc, char *argv[], char *envp[]);
 #endif // BOOST_OS_WINDOWS
 
 #if BOOST_OS_WINDOWS
-extern void (*make_master_vm)(app_context& appctx, asio::io_context& ioctx);
-static void make_master_vm2(app_context& appctx, asio::io_context& ioctx)
+extern void (*make_master_vm)(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx);
+static void make_master_vm2(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #else // BOOST_OS_WINDOWS
-void make_master_vm(app_context& appctx, asio::io_context& ioctx)
+void make_master_vm(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #endif // BOOST_OS_WINDOWS
 {
     auto vm_ctx = make_vm(
-        appctx, ioctx, ContextType::main,
+        appctx, *ioctx, ContextType::main,
         fs::path{"/dev/null/NUL/app/init.lua", fs::path::generic_format});
+    vm_ctx->ioctxref = ioctx;
     appctx.master_vm = vm_ctx;
     vm_ctx->strand().post([vm_ctx]() {
         vm_ctx->fiber_resume(

tool/genmods.cpp
@@ -214,15 +214,19 @@ int main(int argc, char *argv[], char *envp[]);
 #endif // BOOST_OS_WINDOWS
 
 #if BOOST_OS_WINDOWS
-extern void (*make_master_vm)(app_context& appctx, asio::io_context& ioctx);
-static void make_master_vm2(app_context& appctx, asio::io_context& ioctx)
+extern void (*make_master_vm)(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx);
+static void make_master_vm2(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #else // BOOST_OS_WINDOWS
-void make_master_vm(app_context& appctx, asio::io_context& ioctx)
+void make_master_vm(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #endif // BOOST_OS_WINDOWS
 {
     auto vm_ctx = make_vm(
-        appctx, ioctx, ContextType::main,
+        appctx, *ioctx, ContextType::main,
         fs::path{"/dev/null/NUL/app/init.lua", fs::path::generic_format});
+    vm_ctx->ioctxref = ioctx;
     appctx.master_vm = vm_ctx;
     vm_ctx->strand().post([vm_ctx]() {
         vm_ctx->fiber_resume(

test/builtin_modules1.cpp
@@ -41,15 +41,19 @@ int main(int argc, char *argv[], char *envp[]);
 #endif // BOOST_OS_WINDOWS
 
 #if BOOST_OS_WINDOWS
-extern void (*make_master_vm)(app_context& appctx, asio::io_context& ioctx);
-static void make_master_vm2(app_context& appctx, asio::io_context& ioctx)
+extern void (*make_master_vm)(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx);
+static void make_master_vm2(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #else // BOOST_OS_WINDOWS
-void make_master_vm(app_context& appctx, asio::io_context& ioctx)
+void make_master_vm(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #endif // BOOST_OS_WINDOWS
 {
     auto vm_ctx = make_vm(
-        appctx, ioctx, ContextType::main,
+        appctx, *ioctx, ContextType::main,
         fs::path{"/NUL/app/main.lua", fs::path::generic_format});
+    vm_ctx->ioctxref = ioctx;
     appctx.master_vm = vm_ctx;
     vm_ctx->strand().post([vm_ctx]() {
         vm_ctx->fiber_resume(

test/builtin_modules2.cpp
@@ -118,15 +118,19 @@ int main(int argc, char *argv[], char *envp[]);
 #endif // BOOST_OS_WINDOWS
 
 #if BOOST_OS_WINDOWS
-extern void (*make_master_vm)(app_context& appctx, asio::io_context& ioctx);
-static void make_master_vm2(app_context& appctx, asio::io_context& ioctx)
+extern void (*make_master_vm)(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx);
+static void make_master_vm2(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #else // BOOST_OS_WINDOWS
-void make_master_vm(app_context& appctx, asio::io_context& ioctx)
+void make_master_vm(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 #endif // BOOST_OS_WINDOWS
 {
     auto vm_ctx = make_vm(
-        appctx, ioctx, ContextType::main,
+        appctx, *ioctx, ContextType::main,
         fs::path{"/NUL/app/main.lua", fs::path::generic_format});
+    vm_ctx->ioctxref = ioctx;
     appctx.master_vm = vm_ctx;
     vm_ctx->strand().post([vm_ctx]() {
         vm_ctx->fiber_resume(

test/libc_service_read_dev_null.cpp
@@ -128,11 +128,13 @@ namespace main {
 
 int main(int argc, char *argv[], char *envp[]);
 
-void make_master_vm(app_context& appctx, asio::io_context& ioctx)
+void make_master_vm(
+    app_context& appctx, std::shared_ptr<asio::io_context> ioctx)
 {
     auto vm_ctx = make_vm(
-        appctx, ioctx, ContextType::main,
+        appctx, *ioctx, ContextType::main,
         fs::path{"/app/main.lua", fs::path::generic_format});
+    vm_ctx->ioctxref = ioctx;
     appctx.master_vm = vm_ctx;
     vm_ctx->strand().post([vm_ctx]() {
         vm_ctx->fiber_resume(