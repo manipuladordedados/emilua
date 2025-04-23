59a6a717c7a3a0638d40d26c1240f6002c02b952
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2025-04-23 01:20:54 -0300

    Fix test/libpsx1.lua
    Now we synchronize against stdout. Before the patch, I managed to see
    "11" in the test output.

--- test/libpsx1.lua
+++ test/libpsx1.lua
@@ -7,6 +7,19 @@ local stream = require 'stream'
 local system = require 'system'
 local inbox = require 'inbox'
 
+local function print_no_new_privs()
+    local status = stream.scanner.new{ field_separator = ':\t' }
+    status.stream = serial_port.new()
+    status.stream:assign(fs.open(fs.path.new('/proc/thread-self/status'), {'read_only'}))
+    while true do
+        local fields = status:get_line()
+        if tostring(fields[1]) == 'NoNewPrivs' then
+            print(fields[2])
+            break
+        end
+    end
+end
+
 if _CONTEXT == 'main' then
     local vm2 = spawn_vm{
         module = '.',
@@ -16,19 +29,10 @@ if _CONTEXT == 'main' then
     }
     vm2:send(inbox)
     inbox:receive()
+    print_no_new_privs()
 else
     system.set_no_new_privs()
+    print_no_new_privs()
     local vm1 = inbox:receive()
     vm1:send(true)
-end
-
-local status = stream.scanner.new{ field_separator = ':\t' }
-status.stream = serial_port.new()
-status.stream:assign(fs.open(fs.path.new('/proc/thread-self/status'), {'read_only'}))
-while true do
-    local fields = status:get_line()
-    if tostring(fields[1]) == 'NoNewPrivs' then
-        print(fields[2])
-        break
-    end
 end
